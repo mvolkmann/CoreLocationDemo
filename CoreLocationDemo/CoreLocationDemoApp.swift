d53099133ec5ea69695d69ac783842a837540bca
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-01-11 12:51:14 -0600

    Initial Commit

---
+++ CoreLocationDemo/CoreLocationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocationDemoApp.swift
+//  CoreLocationDemo
+//
+//  Created by Mark Volkmann on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
